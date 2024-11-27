e7ed038e0a190feeeb9cf6f3deecdecff7b0fdc3
Joseph <147633570+Joseph-0147@users.noreply.github.com>
2024-11-27 15:05:42 +0300

    Add files via upload

---
+++ src/AssignmentSolutions/InsufficientFundsException.java
@@ -0,0 +1,7 @@
+package AssignmentSolutions;
+
+public class InsufficientFundsException extends Exception {
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+}
