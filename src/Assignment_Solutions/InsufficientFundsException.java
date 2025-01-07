bbb793e4880cc1f0beeec3805d9dfa9a47b8bf97
Joseph <147633570+Joseph-0147@users.noreply.github.com>
2025-01-07 13:01:13 +0300

    Update InsufficientFundsException.java

--- src/Assignment_Solutions/InsufficientFundsException.java
+++ src/Assignment_Solutions/InsufficientFundsException.java
@@ -1,6 +1,23 @@
 package Assignment_Solutions;
 
+/**
+ * InsufficientFundsException
+ 
+ * This exception is thrown when a transaction cannot be completed due to insufficient funds
+ * in the associated bank account.
+ 
+   @author Joseph Kariuki
+ */
 public class InsufficientFundsException extends Exception {
+
+    /**
+     * Constructor: InsufficientFundsException
+     * Initializes the exception with a specific error message.
+     
+     * @param message String - A descriptive message indicating the reason for the exception.
+     * Produces:
+     *  - An exception object with the provided error message.
+     */
     public InsufficientFundsException(String message) {
         super(message);
     }
